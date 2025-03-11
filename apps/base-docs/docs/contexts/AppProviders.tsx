0bf23592a3cf087c8f3bcd3385606884a4643e3e
Ae0h <dontlisten33@gmail.com>
2025-03-11 09:51:12 -0700

    Demo patch

--- apps/base-docs/docs/contexts/AppProviders.tsx
+++ apps/base-docs/docs/contexts/AppProviders.tsx
@@ -1,13 +1,15 @@
 import { lazy, Suspense } from 'react';
 
-const CookieBannerWrapper = lazy(async () => import('./CookieBannerWrapper.tsx'));
+// const CookieBannerWrapper = lazy(async () => import('./CookieBannerWrapper.tsx'));
+import pkg from '@coinbase/cookie-banner';
+const { CookieBanner } = pkg;
 
 export function AppProviders({ children }: { children: React.ReactNode }) {
   return (
     <>
       {children}
       <Suspense fallback={null}>
-        <CookieBannerWrapper />
+        <CookieBanner />
       </Suspense>
     </>
   );
